0e1ddf4fd098ca683394096e6c0abeeea366b0af
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-08-29 22:28:09 +0900

    Initial Commit

---
+++ matchGame/matchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchGameApp.swift
+//  matchGame
+//
+//  Created by 水元太陽 on 2024/08/29.
+//
+
+import SwiftUI
+
+@main
+struct matchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
